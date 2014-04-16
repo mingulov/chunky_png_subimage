70c4e1800cf306beebeb5c3a8a22b2f08c0ea76b
Denis Mingulov <denis@mingulov.com>
2014-04-16 14:58:54 +0300

    Working extensions

--- ext/chunky_png_subimage/chunky_png_subimage.c
+++ ext/chunky_png_subimage/chunky_png_subimage.c
@@ -1,12 +1,168 @@
 #include "chunky_png_subimage.h"
 
-VALUE search_subimage(VALUE self, VALUE args) {
-	return Qnil;
+static inline int is_color_usable(unsigned int color)
+{
+	return (color & 0xff) == 0xff;
+}
+
+static VALUE search_single_subimage(const unsigned int *img, const int imgWidth, const int imgHeight,
+		const int searchX, const int searchY, const int searchWidth, const int searchHeight,
+		const unsigned int *sub, const int subWidth, const int subHeight, int singleMode)
+{
+	VALUE result = rb_ary_new();
+
+	int s1y = 0;
+	int s1x = 0;
+
+	/* search 1st point usable to do the main search */
+	for (s1y = 0; s1y < subHeight; s1y++)
+	{
+		for (s1x = 0; s1x < subWidth; s1x++)
+		{
+			if (is_color_usable(sub[s1y * subWidth + s1x]))
+				break;
+		}
+		if (s1x != subWidth)
+			break;
+	}
+
+	if (s1y == subHeight)
+		rb_raise(rb_eStandardError, "Empty subimage is provided, no usable pixels");
+
+	const unsigned int c = sub[s1y * subWidth + s1x];
+
+	for (int y = searchY; y < searchY + searchHeight; y++)
+	{
+		int x = 0;
+		for (x = searchX; x < searchX + searchWidth; x++)
+		{
+			if (img[(y + s1y) * imgWidth + x + s1x] == c)
+			{
+				/* found 1st possible point, do the full check */
+				int sy = 0;
+				for (sy = s1y; sy < subHeight; sy++)
+				{
+					int sx = 0;
+					for (sx = (sy == s1y) ? s1x : 0; sx < subWidth; sx++)
+					{
+						const unsigned int sc = sub[sy * subWidth + sx];
+						if (is_color_usable(sc) && sc != img[(y + sy) * imgWidth + x + sx])
+							break;
+					}
+					if (sx != subWidth)
+						break;
+				}
+
+				if (sy == subHeight)
+				{
+					/* subimage is found */
+					VALUE coord = rb_ary_new();
+					rb_ary_push(coord, INT2FIX(x));
+					rb_ary_push(coord, INT2FIX(y));
+					rb_ary_push(result, coord);
+
+					if (singleMode & 2)
+						break;
+				}
+			}
+		}
+		/* if 'break' has been used */
+		if (x != searchX + searchWidth)
+			break;
+	}
+
+	return result;
+}
+
+/* convert image to int*, xfree must be used later */
+static unsigned int *convert_image(const VALUE *imgPixels, const int imgWidth, const int imgHeight)
+{
+	unsigned int *imgReady = xmalloc(sizeof(unsigned int) * imgWidth * imgHeight);
+	for(int i = 0; i < imgHeight * imgWidth; i++)
+	{
+		imgReady[i] = FIX2UINT(imgPixels[i]);
+	}
+
+	return imgReady;
+}
+
+VALUE search_subimages(int argc, VALUE *argv, VALUE self)
+{
+	/*
+	params = [img.pixels, img.width, img.height, x, y, width, height, single, subimages.length]
+	subimages.each do |s|
+		params << s.pixels
+		params << s.width
+		params << s.height
+	end
+	 */
+
+	if (argc < 12 || (argc - 9) % 3 != 0)
+		rb_raise(rb_eArgError, "wrong number of arguments");
+
+	VALUE result = rb_ary_new();
+
+	const VALUE *imgPixels = RARRAY_PTR(argv[0]);
+	int imgWidth = FIX2INT(argv[1]);
+	int imgHeight = FIX2INT(argv[2]);
+	int searchX = FIX2INT(argv[3]);
+	int searchY = FIX2INT(argv[4]);
+	int searchWidth = FIX2INT(argv[5]);
+	int searchHeight = FIX2INT(argv[6]);
+	int singleMode = FIX2INT(argv[7]);
+	int amountSubimages = FIX2INT(argv[8]);
+
+	/* some parameters checks */
+	if (searchX < 0)
+		searchX = 0;
+	if (searchY < 0)
+		searchY = 0;
+	if (searchX >= imgWidth)
+		searchX = imgWidth - 1;
+	if (searchY >= imgHeight)
+		searchY = imgHeight - 1;
+	if (searchWidth <= 0)
+		searchWidth = imgWidth;
+	if (searchHeight <= 0)
+		searchHeight = imgHeight;
+	if (searchX + searchWidth >= imgWidth)
+		searchWidth = imgWidth - searchX;
+	if (searchY + searchHeight >= imgHeight)
+		searchHeight = imgHeight - searchY;
+
+	/* prepare image */
+	const unsigned int *imgReady = convert_image(imgPixels, imgWidth, imgHeight);
+
+	int cur = 9;
+	for (int i = 0; i < amountSubimages; i++)
+	{
+		/* cur++ inside RARRAY_PTR might not be used as it is used in the macro multiple times */
+		const VALUE *subPixels = RARRAY_PTR(argv[cur]);
+		int subWidth = FIX2INT(argv[cur + 1]);
+		int subHeight = FIX2INT(argv[cur + 2]);
+		cur += 3;
+		const unsigned int *subReady = convert_image(subPixels, subWidth, subHeight);
+
+		VALUE r = search_single_subimage(imgReady, imgWidth, imgHeight,
+				searchX, searchY, searchWidth, searchHeight,
+				subReady, subWidth, subHeight, singleMode);
+
+		xfree((void *)subReady);
+
+		rb_ary_push(result, r);
+
+		if ((singleMode & 1) && RARRAY_LEN(r) > 0)
+			break;
+	}
+
+	xfree((void *)imgReady);
+
+	return result;
 }
 
 void Init_chunky_png_subimage()
 {
 	VALUE mainModule = rb_define_module("ChunkyPNGSubimage");
 
-	rb_define_singleton_method(mainModule, "_search_subimage", search_subimage, -2);
+	rb_define_singleton_method(mainModule, "_search_subimage", search_subimages, -1);
 }
--- lib/chunky_png_subimage/search.rb
+++ lib/chunky_png_subimage/search.rb
@@ -3,17 +3,27 @@ require 'chunky_png_subimage/chunky_png_subimage'
 
 module ChunkyPNGSubimage
   #
-  def self.search_subimage img, subimages, single = false, x = 0, y = 0, width = 0, height = 0
+  def self.search_subimage img, subimages, single = :single_same_subimage, x = 0, y = 0, width = 0, height = 0
     many_subimages = subimages.is_a? Array
     subimages = [subimages] unless many_subimages
+    case single
+    when :single_everywhere
+      single = 3
+    when :single_same_subimage # just 1 result is needed per subimage
+      single = 2
+    when :single_subimage # single image to find
+      single = 1
+    else
+      single = 0
+    end
     params = [img.pixels, img.width, img.height, x, y, width, height, single, subimages.length]
     subimages.each do |s|
       params << s.pixels
       params << s.width
       params << s.height
     end
-    res = self._search_subiamge *params
-    res = res[0] unless many_subimages
+    res = self._search_subimage *params
+    res = res[0] if (!many_subimages && res.is_a?(Array))
     res
   end
 end
