c6030e3526ba2f641f70dc9296ebb10f5afc1f30
Denis Mingulov <denis@mingulov.com>
2014-04-16 12:28:19 +0300

    Improved ruby method

--- lib/chunky_png_subimage/search.rb
+++ lib/chunky_png_subimage/search.rb
@@ -4,6 +4,13 @@ require 'chunky_png_subimage/chunky_png_subimage'
 module ChunkyPNGSubimage
   #
   def self.search_subimage img, subimages, single = false, x = 0, y = 0, width = 0, height = 0
-    puts self._search_subimage 1
+    subimages = [subimages] unless subimages.is_a? Array
+    params = [img.pixels, img.width, img.height, x, y, width, height, single]
+    subimages.each do |s|
+      params << s.pixels
+      params << s.width
+      params << s.height
+    end
+    self._search_subiamge *params
   end
 end
